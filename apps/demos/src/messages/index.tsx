a74297e36674e0b25321d39757ef7fdb3d87e2ef
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2024-09-16 16:20:14 -0600

    getting shadcn components included

--- apps/demos/src/messages/index.tsx
+++ apps/demos/src/messages/index.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@repo/ui/components/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@repo/ui/components/dialog";
 export const MessagesDemoPage = () => {
   return (
     <div>
       <h1>Messages Demo Page</h1>
+      <Button>Does it work</Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Dialog</Button>
+        </DialogTrigger>
+        <DialogContent>Hello</DialogContent>
+      </Dialog>
     </div>
   );
 };
