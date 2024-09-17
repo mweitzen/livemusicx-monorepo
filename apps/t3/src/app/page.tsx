3f8959224df3ac696c4fe9d75ad762ee98d703b0
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2024-09-16 21:40:07 -0600

    trpc auth working in t3 demo

---
+++ apps/t3/src/app/page.tsx
@@ -0,0 +1,31 @@
+import { Suspense } from "react";
+
+import { api, HydrateClient } from "~/trpc/server";
+import { AuthShowcase } from "./_components/auth-showcase";
+
+export const runtime = "edge";
+
+export default function HomePage() {
+  // You can await this here if you don't want to show Suspense fallback below
+  // void api.post.all.prefetch();
+
+  return (
+    <HydrateClient>
+      <main className='container h-screen py-16'>
+        <div className='flex flex-col items-center justify-center gap-4'>
+          <h1 className='text-5xl font-extrabold tracking-tight sm:text-[5rem]'>
+            Create <span className='text-primary'>T3</span> Turbo
+          </h1>
+          <AuthShowcase />
+
+          <div className='w-full max-w-2xl overflow-y-scroll'>
+            <Suspense
+              fallback={<div className='flex w-full flex-col gap-4'></div>}
+            ></Suspense>
+          </div>
+          <p>a</p>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
