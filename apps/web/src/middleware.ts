4b21bc826b8c8977aada0d85fff9abef9663c02d
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2024-09-18 13:21:27 -0600

    updating

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -5,36 +5,67 @@ import { UserRole } from "@/auth";
 const marketingPages = ["/", "/about", "/pricing", "/features"];
 const appPages = ["/manage", "/explore", "/me"];
 
-export default auth(({ auth, nextUrl, cookies }) => {
-  const { pathname } = nextUrl;
-  const hasVisitedApp = cookies.get("visitedApp")?.value === "true";
+// export default auth(({ auth, nextUrl, cookies }) => {
+//   const { pathname } = nextUrl;
+//   const hasVisitedApp = cookies.get("visitedApp")?.value === "true";
 
-  if (!auth && pathname === "/manage") {
-    return NextResponse.redirect(new URL("/explore", nextUrl));
-  }
-  // Control who sees marketing pages
-  if (marketingPages.includes(pathname)) {
-    // Redirect authenticated users
-    if (!!auth) {
-      if (auth.user.role === UserRole.ADMIN) {
-        return NextResponse.redirect(new URL("/manage", nextUrl));
-      }
-      return NextResponse.redirect(new URL("/explore", nextUrl));
-    }
-    // Redirect unauthenticated users already visited to app
-    if (hasVisitedApp) {
-      return NextResponse.redirect(new URL("/explore", nextUrl));
-    }
-  }
+//   // NOT AUTHENTICATED
+//   if (!auth) {
+//     // Protect app pages
+//     const signInPage = "/api/auth/signin";
+//     const signInUrl = request.nextUrl.clone();
+//     signInUrl.pathname = signInPage;
+//     signInUrl.searchParams.set("callbackUrl", request.nextUrl.pathname);
 
-  // Set visited cookie
-  if (appPages.includes(pathname) && !hasVisitedApp) {
-    const response = NextResponse.next();
-    // TODO: Uncomment
-    // response.cookies.set("visitedApp", "true");
-    return response;
-  }
-});
+//     if (pathname.startsWith("/manage") || pathname.startsWith("/me")) {
+//       return NextResponse.redirect(signInUrl);
+//     }
+//   } else {
+//     // AUTHENTICATED
+//     const adminUser = auth.user.role === "ADMIN";
+//     const redirectToPage = adminUser ? "/manage" : "/me";
+
+//     // Hide auth pages
+//     if (["/signin", "/signup", "/verify"].includes(pathname)) {
+//       return NextResponse.redirect(
+//         new URL(redirectToPage, request.nextUrl)
+//       );
+//     }
+
+//     // Protect admin pages
+//     if (pathname.startsWith("/manage") && !adminUser) {
+//       return NextResponse.redirect(
+//         new URL(redirectToPage, request.nextUrl)
+//       );
+//     }
+//   }
+
+//   if (!auth && pathname === "/manage") {
+//     return NextResponse.redirect(new URL("/explore", nextUrl));
+//   }
+//   // Control who sees marketing pages
+//   if (marketingPages.includes(pathname)) {
+//     // Redirect authenticated users
+//     if (!!auth) {
+//       if (auth.user.role === UserRole.ADMIN) {
+//         return NextResponse.redirect(new URL("/manage", nextUrl));
+//       }
+//       return NextResponse.redirect(new URL("/explore", nextUrl));
+//     }
+//     // Redirect unauthenticated users already visited to app
+//     if (hasVisitedApp) {
+//       return NextResponse.redirect(new URL("/explore", nextUrl));
+//     }
+//   }
+
+//   // Set visited cookie
+//   if (appPages.includes(pathname) && !hasVisitedApp) {
+//     const response = NextResponse.next();
+//     // TODO: Uncomment
+//     // response.cookies.set("visitedApp", "true");
+//     return response;
+//   }
+// });
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets|avatars).*)"],
