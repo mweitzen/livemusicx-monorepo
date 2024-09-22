233db446d3c31ee233ebc09934103a076cf04358
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2024-09-22 00:13:05 -0600

    adding mock data

---
+++ libs/mock-data/index.ts
@@ -0,0 +1,9 @@
+export * from "./mock-accounts";
+export * from "./mock-notifications";
+export * from "./mock-search-items";
+
+export * from "./mock-events";
+export * from "./mock-venues";
+export * from "./mock-musicians";
+export * from "./mock-bands";
+export * from "./mock-organizers";
