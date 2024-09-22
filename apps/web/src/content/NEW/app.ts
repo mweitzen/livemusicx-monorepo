3e6c6eb344e6712c516ae95568b29c6df48939ab
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2024-09-21 23:42:31 -0600

    refactoring

--- apps/web/src/content/NEW/app.ts
+++
@@ -1,6 +0,0 @@
-export const content = {
-  title: "Live Music X",
-  slogan: "Empowering Local Sounds, Connecting Vibrant Communities",
-  description:
-    "Uncover hidden gems and vibrant local music scenes with Live Music X. Find concerts, open mics, and more happening near you. Connect with artists, venues, and fellow music lovers in your community. Never miss a beat!",
-};
